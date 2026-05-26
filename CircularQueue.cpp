a5c7db41b58c7e8bf0fd597e58ae8e2a5a511cb2
taufiq72 <taufiqq1207@gmail.com>
2026-05-26 10:31:22 +0700

    membuat else cek antrian kosong

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -35,5 +35,13 @@ public:
             FRONT = 0;
             REAR = 0;
         }
+        else
+        {
+            if (REAR == max -1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 };
